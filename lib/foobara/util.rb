bda42d730713da4994972d3ac40aa5883744fc7f
Miles Georgi <azimux@gmail.com>
2023-07-19 03:51:48 +0000

    move power_set to Util

--- lib/foobara/util.rb
+++ lib/foobara/util.rb
@@ -6,5 +6,13 @@ module Foobara
       name = mod.name[/(.*)::/, 1]
       Object.const_get(name) if name
     end
+
+    def power_set(array)
+      return [[]] if array.empty?
+
+      head, *tail = array
+      subsets = power_set(tail)
+      subsets + subsets.map { |subset| [head, *subset] }
+    end
   end
 end
