6fbd76440b2d2b390fbb6b1a2d3ef51dc917608d
Miles Georgi <azimux@gmail.com>
2023-07-21 18:20:35 +0000

    Add Util methods for constant lookup and requiring directories

--- lib/foobara/util.rb
+++ lib/foobara/util.rb
@@ -14,5 +14,33 @@ module Foobara
       subsets = power_set(tail)
       subsets + subsets.map { |subset| [head, *subset] }
     end
+
+    def constant_value(mod, constant)
+      if mod.constants(false).include?(constant)
+        mod.const_get(constant, false)
+      end
+    end
+
+    def constant_values(mod, klasses_to_include = nil)
+      klasses_to_include = klasses_to_include ? Array.wrap(klasses_to_include) : [Object]
+
+      mod.constants.map { |const| constant_value(mod, const) }.select do |object|
+        klasses_to_include.any? { |klass| object.is_a?(klass) }
+      end
+    end
+
+    def require_directory(directory)
+      require_pattern("#{directory}/**/*.rb")
+    end
+
+    def require_pattern(glob)
+      files = Dir[glob].sort_by(&:length).reverse
+
+      raise "Didn't find anything to require for #{glob}" if files.empty?
+
+      files.each do |f|
+        require f
+      end
+    end
   end
 end
