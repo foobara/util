c7c0478f123f1a7b1afa7faaadc4302ca8c4d58b
Miles Georgi <azimux@gmail.com>
2023-08-15 14:55:03 -0700

    Add .arg_and_opts_to_arg and .args_and_opts_to_args

--- lib/foobara/common/util.rb
+++ lib/foobara/common/util.rb
@@ -75,5 +75,46 @@ module Foobara
 
       (args.first || {}).merge(opts)
     end
+
+    def arg_and_opts_to_arg(arg, opts)
+      if arg.present?
+        if opts.present?
+          unless opts.is_a?(Hash)
+            raise ArgumentError, "opts must be a hash not a #{opts}"
+          end
+
+          unless arg.is_a?(Hash)
+            raise ArgumentError, "arg must be a hash if present when opts is present"
+          end
+
+          arg.merge(opts)
+        else
+          arg
+        end
+      else
+        opts.presence
+      end
+    end
+
+    # Strange utility method here...
+    # idea is there's a method that takes 0 or 1 arguments which may or may not be a hash.
+    # To make sure we don't act like there's an argument when there's not or fail to combine
+    # parts of the hash that wind up in opts, this method comines it into an array of 0 or 1
+    # argument with stuff merged into the argument if needed.
+    def args_and_opts_to_args(args, opts)
+      unless args.is_a?(Array)
+        raise ArgumentError, "args must be an array of 0 or 1 hashes but received #{args}"
+      end
+
+      case args.size
+      when 0
+        Array.wrap(arg_and_opts_to_arg(args.first, opts))
+      when 1
+        # Do not go from 1 argument to 0. ie, [nil] should return [nil] not [].
+        [arg_and_opts_to_arg(args.first, opts)]
+      else
+        raise ArgumentError, "args must be an array of 0 or 1 hashes but received #{args}"
+      end
+    end
   end
 end
