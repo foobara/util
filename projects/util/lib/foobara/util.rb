a7e9ddab2a2e15d6fb170eeeb01d52a7238e7682
Miles Georgi <azimux@gmail.com>
2023-09-22 21:39:27 +0000

    Eliminate #symbolize_keys

--- projects/util/lib/foobara/util.rb
+++ projects/util/lib/foobara/util.rb
@@ -172,6 +172,26 @@ module Foobara
       require_relative("../../../#{project}/src/#{path}")
     end
 
+    def symbolize_keys(hash)
+      unless all_symbolizable_keys?(hash)
+        # :nocov:
+        raise "Cannot symbolize keys for #{hash} because they are not all symbolizable"
+        # :nocov:
+      end
+
+      hash.transform_keys(&:to_sym)
+    end
+
+    def symbolize_keys!(hash)
+      unless all_symbolizable_keys?(hash)
+        # :nocov:
+        raise "Cannot symbolize keys for #{hash} because they are not all symbolizable"
+        # :nocov:
+      end
+
+      hash.transform_keys!(&:to_sym)
+    end
+
     def all_symbolic_keys?(hash)
       all_symbolic_elements?(hash.keys)
     end
