6999571f9a41d8ee0509ce439f38c98d0e76326a
Miles Georgi <azimux@gmail.com>
2023-12-09 01:05:38 +0000

    Add utility method for finding supermethods

--- projects/util/src/util.rb
+++ projects/util/src/util.rb
@@ -2,6 +2,17 @@ module Foobara
   module Util
     module_function
 
+    # Kind of surprising that Ruby doesn't have a built in way to do this.
+    def super_method_of(current_instance, from_class, method_name)
+      method = current_instance.method(method_name)
+      method = method.super_method until method.owner == from_class
+      method.super_method
+    end
+
+    def super_method_takes_parameters?(current_instance, from_class, method_name)
+      super_method_of(current_instance, from_class, method_name).parameters.any?
+    end
+
     def module_for(mod)
       name = mod.name&.[](/(.*)::/, 1)
       Object.const_get(name) if name
