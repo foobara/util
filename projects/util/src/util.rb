dbd024869b258df1c4e2406f50abf6d530df5b76
Miles Georgi <azimux@gmail.com>
2023-12-15 00:42:48 +0000

    Add some guards and add .parent_module_name_for

--- projects/util/src/util.rb
+++ projects/util/src/util.rb
@@ -14,8 +14,16 @@ module Foobara
     end
 
     def module_for(mod)
-      name = mod.name&.[](/(.*)::/, 1)
+      name = mod.name
+
+      return unless name
+
+      name = parent_module_name_for(mod.name)
       Object.const_get(name) if name
+    end
+
+    def parent_module_name_for(module_name)
+      module_name[/(.*)::/, 1]
     end
 
     def non_full_name(mod)
@@ -397,6 +405,17 @@ module Foobara
       mod.send(:remove_const, name)
     end
 
+    class ParentModuleDoesNotExistError < StandardError
+      attr_reader :name, :parent_name
+
+      def initialize(name:, parent_name:)
+        @name = name
+        @parent_name = parent_name
+
+        super("Parent module #{parent_name} does not exist for #{name}.")
+      end
+    end
+
     def make_class(name, superclass = nil, which: :class, &block)
       name = name.to_s if name.is_a?(::Symbol)
 
@@ -409,6 +428,12 @@ module Foobara
       superclass ||= :Object
 
       name = name[2..] if name.start_with?("::")
+
+      parent_name = parent_module_name_for(name)
+
+      if parent_name && !Object.const_defined?(parent_name)
+        raise ParentModuleDoesNotExistError.new(name:, parent_name:)
+      end
 
       # rubocop:disable Security/Eval, Style/DocumentDynamicEvalDefinition
       eval(<<~RUBY, binding, __FILE__, __LINE__ + 1)
--- spec/foobara/common/util_spec.rb
+++ spec/foobara/common/util_spec.rb
@@ -92,4 +92,14 @@ RSpec.describe Foobara::Util do
       end
     end
   end
+
+  describe ".make_class" do
+    context "when parent modules don't exist" do
+      it "raises an error" do
+        expect {
+          described_class.make_class("A::B")
+        }.to raise_error(described_class::ParentModuleDoesNotExistError)
+      end
+    end
+  end
 end
