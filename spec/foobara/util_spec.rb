d5858c14fda4674fe3db8a3571cfa8108871aa0f
Miles Georgi <azimux@gmail.com>
2024-01-15 12:40:05 -0800

    Create gem boilerplate files with RubyMine

---
+++ spec/foobara/util_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Foobara::Util do
+  it "has a version number" do
+    expect(Foobara::Util::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
