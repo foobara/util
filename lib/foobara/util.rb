42d975675cdfcc62692dd30d9de06085c256eb9d
Miles Georgi <azimux@gmail.com>
2024-01-15 14:48:54 -0800

    mv files out of projects/ to top-level

--- lib/foobara/util.rb
+++ lib/foobara/util.rb
@@ -1,8 +1,23 @@
-require_relative "util/version"
-
 module Foobara
   module Util
-    class Error < StandardError; end
-    # Your code goes here...
+    module_function
+
+    def require_directory(directory)
+      require_pattern("#{directory}/**/*.rb")
+    end
+
+    def require_pattern(glob)
+      files = Dir[glob]
+
+      if files.empty?
+        # :nocov:
+        raise "Didn't find anything to require for #{glob}"
+        # :nocov:
+      end
+
+      files.sort_by { |file| [file.count("/"), file.length] }.reverse.each do |f|
+        require f
+      end
+    end
   end
 end
