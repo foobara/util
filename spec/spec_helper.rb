1f5172a1df07edb0541030d0197d80648c836f8b
Miles Georgi <azimux@gmail.com>
2024-01-15 14:44:15 -0800

    Wire up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,34 @@
-require "foobara/util"
+require "bundler/setup"
+
+require "pry"
+require "pry-byebug"
+require "rspec/its"
+require "simplecov"
+
+SimpleCov.start do
+  # enable_coverage :branch
+  minimum_coverage line: 100
+  # TODO: enable this? worth it to get to 100% branch coverage?
+  # minimum_coverage line: 100, branch: 100
+end
 
 RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
+  config.order = :defined
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  # config.raise_errors_for_deprecations!
 end
+
+require "foobara/util/all"
+require "foobara/spec_helpers/all"
