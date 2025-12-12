13de7cfcb3033bc30bb64bc49f702878ce96a9d5
cancelei <glauber.bannwart@gmail.com>
2025-12-12 14:57:14 -0300

    feat/remove Util namespace for Empty Array

--- lib/foobara/util/constants.rb
+++ lib/foobara/util/constants.rb
@@ -1,7 +1,3 @@
 module Foobara
-  EMPTY_ARRAY = [].freeze unless const_defined?(:EMPTY_ARRAY)
-
-  module Util
-    EMPTY_ARRAY = Foobara::EMPTY_ARRAY unless const_defined?(:EMPTY_ARRAY)
-  end
+  EMPTY_ARRAY = [].freeze
 end
